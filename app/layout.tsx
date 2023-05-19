770896c604e1279a4b471cde0a35b05b808f0085
Asish Mahapatra <asishmahapatra918@gmail.com>
2023-05-19 21:29:29 +0530

    Added and Styled the Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Nunito } from 'next/font/google'
 import './globals.css'
+import Navbar from "./components/navbar/Navbar";
 
 export const metadata = {
     title: 'Airbnb',
@@ -15,6 +16,7 @@ export default function RootLayout({children,}: {children: React.ReactNode}) {
     return (
         <html lang="en">
             <body className={font.className}>
+                <Navbar />
                 {children}
             </body>
         </html>
