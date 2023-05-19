929a92af0e6623cda61a08354fb54c8e3be50472
Asish Mahapatra <asishmahapatra918@gmail.com>
2023-05-20 01:26:50 +0530

    Added and Styled the Auth UI

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,10 +1,12 @@
 'use client'
 import { useCallback, useState } from 'react'
 import { AiOutlineMenu } from 'react-icons/ai'
+import useRegisterModal from '../../hooks/useRegisterModal'
 import Avatar from '../Avatar'
 import MenuItem from './MenuItem'
 
 const UserMenu = () => {
+    const registerModal = useRegisterModal();
     const [isOpen, setIsOpen] = useState(false);
 
     const toggleOpen = useCallback(() => {
@@ -30,7 +32,7 @@ const UserMenu = () => {
                     <div className='flex flex-col cursor-pointer'>
                         <>
                             <MenuItem onClick={() => {}} label="Login" />
-                            <MenuItem onClick={() => {}} label="Sign Up" />
+                            <MenuItem onClick={registerModal.onOpen} label="Sign Up" />
                         </>
                     </div>
                 </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { Nunito } from 'next/font/google'
 import './globals.css'
 import Navbar from "./components/navbar/Navbar";
+import Modal from "./components/modals/Modal";
+import RegisterModal from "./components/modals/RegisterModal";
+import ToasterProvider from "./providers/ToasterProvider";
 
 export const metadata = {
     title: 'Airbnb',
@@ -16,6 +19,8 @@ export default function RootLayout({children,}: {children: React.ReactNode}) {
     return (
         <html lang="en">
             <body className={font.className}>
+                <ToasterProvider />
+                <RegisterModal />
                 <Navbar />
                 {children}
             </body>
