5b70037717a9ceb8e88812f5a1c1e9b4d11db238
Asish Mahapatra <asishmahapatra918@gmail.com>
2023-05-21 18:00:04 +0530

    Implemented 4th section of RentModal

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -11,6 +11,7 @@ import Modal from "./Modal";
 import Map from "../Map";
 import dynamic from "next/dynamic";
 import Counter from "../inputs/Counter";
+import ImageUpload from "../inputs/ImageUpload";
 
 enum STEPS {
     CATEGORY = 0,
@@ -53,6 +54,7 @@ const RentModal = () => {
     const guestCount = watch("guestCount");
     const roomCount = watch("roomCount");
     const bathroomCount = watch("bathroomCount");
+    const imageSrc = watch("imageSrc");
 
     const Map = useMemo(() => 
         dynamic(() => import("../Map"), { ssr: false }
@@ -160,6 +162,22 @@ const RentModal = () => {
                     onChange={(value) => setCustomValue('bathroomCount', value)}
                 />
 
+            </div>
+        )
+    }
+
+    if(step === STEPS.IMAGES){
+        bodyContent = (
+            <div className="flex flex-col gap-8">
+                <Heading 
+                    title="Add a photo of your place"
+                    subtitle="Show guests what your place look like!"
+                />
+
+                <ImageUpload 
+                    value={imageSrc}
+                    onChange={(value) => setCustomValue("imageSrc", value)}
+                />
             </div>
         )
     }
